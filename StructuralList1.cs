152d6cf2783466f94eceb21078a62770c29cf7c0
AugustoBarcelos <78539991+AugustoBarcelos@users.noreply.github.com>
2021-03-23 18:52:12 -0300

    Create StructuralList1.cs

---
+++ StructuralList1.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter first number:");
+		int first = int.Parse(Console.ReadLine());	
+		Console.WriteLine("Enter second number:");
+		int sec = int.Parse(Console.ReadLine());
+				
+		Console.WriteLine("SUM:"+first+sec);
+	
+
+	}
+}
