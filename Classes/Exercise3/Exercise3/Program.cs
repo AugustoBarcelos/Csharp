6cc02ab2197e1109893f856a234b49a1a55ee6f6
AugustoBarcelos <78539991+AugustoBarcelos@users.noreply.github.com>
2021-03-28 17:23:03 -0300

    Add files via upload

---
+++ Classes/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student aluno = new student ();
+            Console.WriteLine("Enter the student's Name:");
+            aluno.Name = Console.ReadLine();
+            Console.WriteLine("Enter the 1st grade:");
+            aluno.grade1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the 2nd grade:");
+            aluno.grade2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the 3rd grade:");
+            aluno.grade3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your Final Grade is: " +aluno.finalgrade().ToString("F2"));
+            
+            if (aluno.Aproved())
+            {
+                Console.WriteLine("Approved");
+            }
+            else
+            {
+                Console.WriteLine("Reproved");
+                Console.WriteLine("It was missing: "+aluno.missing().ToString("F2"));
+            }
+        }
+    }
+}
---
+++ Classes/Exercise3/Exercise3/student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class student
+    {
+        public string Name;
+        public double grade1, grade2, grade3;
+
+        public double finalgrade()
+        {
+            return grade1 + grade2 + grade3;
+        }
+        public bool Aproved()
+        {
+            if (finalgrade() > 59.4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double missing()
+            {
+            if (Aproved ())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - finalgrade();
+            }
+            }
+        }
+
+    }
+
