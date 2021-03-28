07e268f987a7100ec19ac4d4d1c27e1ff5b4c899
AugustoBarcelos <78539991+AugustoBarcelos@users.noreply.github.com>
2021-03-28 18:46:07 -0300

    Add files via upload

---
+++ Bank System/ConsoleApp1/ConsoleApp1/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Account
+    {
+        public int AccountNumber { get; private set; }
+        public string Owner { get; set; }
+        public double Balance { get; private set; }
+
+        public Account(int accountNumber, string owner, double balance)
+        {
+            AccountNumber = accountNumber;
+            Owner = owner;
+            Balance = balance;
+        }
+        public void deposit (double ammount)
+        {
+            Balance += ammount;
+        }
+        public void withdraw (double ammount)
+        {
+            Balance -= ammount + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Account: "
+                + AccountNumber
+                + ", Owner:"
+                + Owner
+                + ", Balance: US$ "
+                + Balance.ToString("F2", CultureInfo.InvariantCulture);        }
+    }
+}
---
+++ Bank System/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account;
+            Console.Write("Enter the account number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Enter the account owner:");
+            string name = Console.ReadLine();
+            Console.Write("Is there any initial fund?(y/n):");
+            char answer = char.Parse(Console.ReadLine());
+            if (answer=='y' || answer == 'Y'){
+                Console.WriteLine("Enter the initial fund please:");
+                double fund = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                account = new Account(number, name, fund);
+            }
+            else
+            {
+                account = new Account(number, name,0);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Account Details:");
+            Console.WriteLine(account);
+
+            Console.WriteLine("Enter the ammount of the deposit: ");
+            double ammount = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            account.deposit(ammount);
+            Console.WriteLine("Updated account info:");
+            Console.WriteLine(account);
+
+            Console.WriteLine("Enter the ammount of the withdraw: ");
+            double with = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.withdraw(with);
+            Console.WriteLine("Updated account info:");
+            Console.WriteLine(account);
+        }
+    }
+}
