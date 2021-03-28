5c18dcf257ce5c1f20f8f69a05a122f6014e341a
AugustoBarcelos <78539991+AugustoBarcelos@users.noreply.github.com>
2021-03-28 10:59:14 -0300

    Add files via upload

---
+++ Classes/Second List/Exercises1secondlist/Exercises1secondlist/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercises1secondlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangle ret = new Retangle();
+            Console.WriteLine("Enter the rectangle's width and height of the triangle:");
+            ret.Width = double.Parse(Console.ReadLine());
+            ret.Height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area = " + ret.Area().ToString("F2")) ;
+            Console.WriteLine("Diagonal = " + ret.Diagonal().ToString("F2")) ;
+            Console.WriteLine("Permiter = " + ret.Perimeter().ToString("F2")) ;
+
+        }
+    }
+}
---
+++ Classes/Second List/Exercises1secondlist/Exercises1secondlist/Retangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises1secondlist
+{
+    class Retangle
+    {
+        public double Height;
+        public double Width;
+
+        public double Area()
+        {
+            return Height * Width;
+        }
+        public double Perimeter()
+        {
+            return (Height * 2)+(2*Width);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt((Height * Height) + (Width * Width));
+                ;
+        }
+    }
+}
